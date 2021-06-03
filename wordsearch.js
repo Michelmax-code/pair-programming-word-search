239c7e11ac968d343aa7436ac2de22a18d55b469
Miguel Jimenez <migang9@gmail.com>
2021-06-03 15:33:26 -0400

    Word search pair programming with Andyiev

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const verticalJoin = transposition(letters).map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (k of verticalJoin) {
+    if (k.includes(word)) return true;
+  }
+  return false;
+};
 
-module.exports = wordSearch
+const transposition = function(matrix) {
+  let result = [];
+  let newArray = [];
+  //let row - i - matrix.length
+  //let columns - j - matrix[0].length
+  for (let column = 0; column < matrix[0].length; column++) {
+    for (let row = 0; row < matrix.length; row++) {
+      newArray.push(matrix[row][column]);
+    }
+    result.push(newArray);
+    newArray = [];
+  }
+  return result;
+};
+  
+module.exports = wordSearch;
