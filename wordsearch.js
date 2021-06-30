a368d73c010076f2ddce443f97d225fb67865b10
Miguel Jimenez <migang9@gmail.com>
2021-06-30 18:21:18 -0400

    add new case

--- wordsearch.js
+++ wordsearch.js
@@ -1,10 +1,10 @@
 const wordSearch = (letters, word) => {
   const horizontalJoin = letters.map(ls => ls.join(''));
   const verticalJoin = transposition(letters).map(ls => ls.join(''));
-  for (l of horizontalJoin) {
+  for (let l of horizontalJoin) {
     if (l.includes(word)) return true;
   }
-  for (k of verticalJoin) {
+  for (let k of verticalJoin) {
     if (k.includes(word)) return true;
   }
   return false;
@@ -15,12 +15,14 @@ const transposition = function(matrix) {
   let newArray = [];
   //let row - i - matrix.length
   //let columns - j - matrix[0].length
+  if (matrix === []) {
+    throw new Error('name is empty');
+  }
   for (let column = 0; column < matrix[0].length; column++) {
     for (let row = 0; row < matrix.length; row++) {
       newArray.push(matrix[row][column]);
     }
     result.push(newArray);
-    newArray = [];
   }
   return result;
 };
